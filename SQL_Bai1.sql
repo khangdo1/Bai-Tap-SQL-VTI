83c0d36e8d2e1a3c9973be66bdec18205c9e988b
khangdo1 <docaokhang@gmail.com>
2023-05-21 11:10:15 +0700

    Lam Bai Tap 1 mon SQL

---
+++ SQL_Bai1.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE Salemanagement;
+USE salemanagement;
+
+CREATE TABLE Customers (
+	Cutomers_id INT,
+    First_nam VARCHAR(50),
+    Date_of_Purchase DATE
+    );
+    
+    CREATE TABLE SALES (
+    Customer_id INT,
+    First_name VARCHAR(50),
+    Date_of_Purchase DATE
+    );
